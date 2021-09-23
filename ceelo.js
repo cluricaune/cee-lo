921315be6135a55ac9139fdeec6976ef658a77c9
cluricaune <dunn.jake90@gmail.com>
2021-09-23 17:02:49 -0400

    begin css upgrade

--- ceelo.js
+++ ceelo.js
@@ -8,26 +8,33 @@ function threeRoll() {
     let dice2 = singleRoll();
     let dice3 = singleRoll();
     let score;
+
+    document.querySelector("#dieface1").src = `/imgs/dicefaces/die${dice1}.png`;
+    document.querySelector("#dieface2").src = `/imgs/dicefaces/die${dice2}.png`;
+    document.querySelector("#dieface3").src = `/imgs/dicefaces/die${dice3}.png`;
     
     document.getElementById("p").textContent = ``;
 
     if (dice1 === dice2 && dice1 === dice3) {
-        document.getElementById("p").textContent += `${dice1}, ${dice2}, ${dice3} -- Trips! Score: Trips ${dice1}`;
+        document.getElementById("p").textContent += `Trips! Score: Trips ${dice1}`;
+        score = dice1 * 3;
     } else if (dice1 === dice2) {
-        document.getElementById("p").textContent += `${dice1}, ${dice2}, ${dice3} -- Score: ${dice3}`;
+        document.getElementById("p").textContent += `Score: ${dice3}`;
         score = dice3;
     } else if (dice1 === dice3) {
-        document.getElementById("p").textContent += `${dice1}, ${dice2}, ${dice3} -- Score: ${dice2}`;
+        document.getElementById("p").textContent += `Score: ${dice2}`;
         score = dice2;
     } else if (dice2 === dice3) {
-        document.getElementById("p").textContent += `${dice1}, ${dice2}, ${dice3} -- Score: ${dice1}`;
+        document.getElementById("p").textContent += `Score: ${dice1}`;
         score = dice1;
     } else if ((dice1 === 1 && dice2 === 2 && dice3 === 3) || (dice1 === 1 && dice2 === 3 && dice3 === 2) || (dice1 === 2 && dice2 === 1 && dice3 === 3) || (dice1 === 2 && dice2 === 3 && dice3 === 1) || (dice1 === 3 && dice2 === 2 && dice3 === 1) || (dice1 === 3 && dice2 === 1 && dice3 === 2)) {
-        document.getElementById("p").textContent += `${dice1}, ${dice2}, ${dice3} -- One, two, three, you lose!`;
+        document.getElementById("p").textContent += `One, two, three, you lose!`;
     } else if ((dice1 === 4 && dice2 === 5 && dice3 === 6) || (dice1 === 4 && dice2 === 6 && dice3 === 5) || (dice1 === 5 && dice2 === 4 && dice3 === 6) || (dice1 === 5 && dice2 === 6 && dice3 === 4) || (dice1 === 6 && dice2 === 5 && dice3 === 4) || (dice1 === 6 && dice2 === 4 && dice3 === 5)) {
-        document.getElementById("p").textContent += `${dice1}, ${dice2}, ${dice3} -- Four, five, six, you win!`;
+        document.getElementById("p").textContent += `Four, five, six, you win!`;
     } else {
-        document.getElementById("p").textContent += `${dice1}, ${dice2}, ${dice3} -- Roll again!`;
+        document.getElementById("p").textContent += `Roll again!`;
     }
 }
-threeRoll();
+
+const rollDice = document.getElementById("rollDice");
+rollDice.addEventListener(`click`, threeRoll);
