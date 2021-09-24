96b7a6cef9ef36852670b20befb7db6ec7c7b3bc
cluricaune <dunn.jake90@gmail.com>
2021-09-24 14:16:41 -0400

    major graphic overhaul

--- ceelo.js
+++ ceelo.js
@@ -12,27 +12,38 @@ function threeRoll() {
     document.querySelector("#dieface1").src = `imgs/dicefaces/die${dice1}.png`;
     document.querySelector("#dieface2").src = `imgs/dicefaces/die${dice2}.png`;
     document.querySelector("#dieface3").src = `imgs/dicefaces/die${dice3}.png`;
-    
-    document.getElementById("p").textContent = ``;
 
     if (dice1 === dice2 && dice1 === dice3) {
-        document.getElementById("p").textContent += `Trips! Score: Trips ${dice1}`;
+        document.getElementById("response").textContent = `Trips!`;
+        document.getElementById("response").style.color = `#00EA11`;
         score = dice1 * 3;
     } else if (dice1 === dice2) {
-        document.getElementById("p").textContent += `Score: ${dice3}`;
+        document.getElementById("response").textContent = `Dubs`;
+        document.getElementById("response").style.color = `#00EA11`;
+        document.getElementById("scorenum").textContent = `${dice3}`;
         score = dice3;
     } else if (dice1 === dice3) {
-        document.getElementById("p").textContent += `Score: ${dice2}`;
+        document.getElementById("response").textContent = `Dubs`;
+        document.getElementById("response").style.color = `#00EA11`;
+        document.getElementById("scorenum").textContent = `${dice2}`;
         score = dice2;
     } else if (dice2 === dice3) {
-        document.getElementById("p").textContent += `Score: ${dice1}`;
+        document.getElementById("response").textContent = `Dubs`;
+        document.getElementById("response").style.color = `#00EA11`;
+        document.getElementById("scorenum").textContent = `${dice1}`;
         score = dice1;
     } else if ((dice1 === 1 && dice2 === 2 && dice3 === 3) || (dice1 === 1 && dice2 === 3 && dice3 === 2) || (dice1 === 2 && dice2 === 1 && dice3 === 3) || (dice1 === 2 && dice2 === 3 && dice3 === 1) || (dice1 === 3 && dice2 === 2 && dice3 === 1) || (dice1 === 3 && dice2 === 1 && dice3 === 2)) {
-        document.getElementById("p").textContent += `One, two, three, you lose!`;
+        document.getElementById("response").textContent = `One, two, three, you lose!`;
+        document.getElementById("response").style.color = `#FF0000`;
+        document.getElementById("scorenum").textContent = `X`;
     } else if ((dice1 === 4 && dice2 === 5 && dice3 === 6) || (dice1 === 4 && dice2 === 6 && dice3 === 5) || (dice1 === 5 && dice2 === 4 && dice3 === 6) || (dice1 === 5 && dice2 === 6 && dice3 === 4) || (dice1 === 6 && dice2 === 5 && dice3 === 4) || (dice1 === 6 && dice2 === 4 && dice3 === 5)) {
-        document.getElementById("p").textContent += `Four, five, six, you win!`;
+        document.getElementById("response").textContent = `Four, five, six, you win!`;
+        document.getElementById("response").style.color = `#00EA11`;
+        document.getElementById("scorenum").textContent = `42069`;
     } else {
-        document.getElementById("p").textContent += `Roll again!`;
+        document.getElementById("response").textContent = `Roll again`;
+        document.getElementById("response").style.color = `#A400E8`;
+        document.getElementById("scorenum").textContent = `X`;
     }
 }
 
